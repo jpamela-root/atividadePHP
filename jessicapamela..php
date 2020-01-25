035756384622a2c2437575c7594bb469901c3d0f
jpamela_root <59771746+jpamela-root@users.noreply.github.com>
2020-01-25 14:47:59 -0300

    Add files via upload

---
+++ jessicapamela..php
@@ -0,0 +1,35 @@
+﻿<?php
+	$palavras = array('estudar', 'educação', 'esforço', 'persistência', 'dedicação', 'crescimento', 'evolução', 'sabedoria', 'trabalho', 'entusiasmo', 'alegria', 'vitória', 'sucesso', 'profissão', 'conhecimento', 'vida');		
+?>
+<html>
+	<head>
+	<title> Projeto 2</title>
+	</head>
+	<body > 
+		<div>
+			<table id="Menor" border="1">
+				<tr><td colspan="2"> LETRAS PEQUENAS </td> </tr>
+				<?php  
+					for ($i=0; $i < count ($palavras); $i++) {
+						$tamanho = strlen(utf8_decode($palavras[$i]));
+						if ($tamanho <=7){
+							echo "<tr><td>".$palavras[$i]."</td><td> ".strlen(utf8_decode($palavras[$i]))."</td></tr>";
+						}
+					}
+				?>
+			</table>
+			<br>
+			<table id="Maior" border="1">
+				<tr><td colspan="2"> LETRAS GRANDES </td> </tr>
+				<?php 
+					for ($i=0; $i < count ($palavras); $i++) {
+						$tamanho = strlen(utf8_decode($palavras[$i]));
+						if ($tamanho >7){
+							echo "<tr><td>".$palavras[$i]."</td><td> ".strlen(utf8_decode($palavras[$i]))."</td></tr>";
+						}
+					}
+				?>
+			</table>
+		</div>
+	</body>
+</html>
